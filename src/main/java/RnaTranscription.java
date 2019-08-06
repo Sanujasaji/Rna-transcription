b4ec80374110a67feb4474af690401971e7b0765
sanuja <sanuja.saji@thoughtworks.com>
2019-08-06 12:07:07 +0530

    First commit

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,7 +1,32 @@
 class RnaTranscription {
 
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    static String transcribe(String dnaStrand) {
+        char c;
+        char[] chars = dnaStrand.toCharArray();
+
+
+        for(int i=0;i<dnaStrand.length();i++){
+            c = dnaStrand.charAt(i);
+            if('G' == chars[i]) {
+                chars[i] = 'C' ;
+            }else if('C' == chars[i]) {
+                chars[i] = 'G' ;
+            }else if('T' == chars[i]) {
+                chars[i] = 'A' ;
+            }else if('A' == chars[i]) {
+                chars[i] = 'U' ;
+            }
+
+        }
+        dnaStrand = String.valueOf(chars);
+        return dnaStrand;
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
     }
+
+    public static void main(String[] args){
+        System.out.println(transcribe("AAAAA"));
+    }
+
 
 }
